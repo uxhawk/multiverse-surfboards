e83fb96fdb6fb6ec825c6870448c2c478cbda55e
uxhawk <uxhawk@gmail.com>
2025-12-16 13:22:16 -0800

    config update for gh pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: "/multiverse-surfboards",
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: "/multiverse-surfboards",
+  output: "export",
 };
 
 export default nextConfig;
